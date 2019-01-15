769b8be87192d489455f72bb10b4b812efd93c00
Kyle Krueger <kyle.s.krueger@gmail.com>
2019-01-15 14:54:25 +0100

    add pipeline and cli

--- setup.py
+++ setup.py
@@ -30,7 +30,12 @@ setup(
         'matplotlib>=2.1.0',
         'pytest>=3.2.3',
         'pytest-runner>=2.12.1',
+        'click>=6.7',
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+        titanic_analysis=titanic.command_line:titanic_analysis
+    '''
 )
